b3f5ae6b7ab687380f4482f2773208e98d183c7a
DanielXu2008 <danxu876@gmail.com>
2025-07-18 14:46:38 -0400

    Add app icon

---
+++ ico/version.h
@@ -0,0 +1,20 @@
+#ifndef _PRODUCT_VERSION
+#define _PRODUCT_VERSION
+
+#define CHESSMD_PRODUCT_VERSION	"0.9.7"
+
+
+
+#define BINARY_VERSION 0,9,7
+
+#define VERSION_TEXT	"0.9.7\0"
+#define APP_NAME		"ChessMD.exe\0"
+#define APP_DESC		"ChessMD - Chess Database Viewer\0"
+#define COMPANY_NAME	"ChessMD\0"
+#define WEB_LINK		"https://chessmd.org\0"
+#define LEGAL_COPYRIGHT	"Copyright 2025-\0"
+#define LANGUAGE_ID		0x809
+
+
+#endif
+
